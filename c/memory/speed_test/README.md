87bf21c0c6ae34ce7c917e8cb84304e2038bc189
netnode <grumirafi@gmail.com>
2025-06-12 22:18:36 +0200

    Super simplte speed test c vs python

---
+++ c/memory/speed_test/README.md
@@ -0,0 +1,6 @@
+### Paragone (semplice) velocità tra C e Python
+Per il file "main.c"
+```
+gcc main.c -o main
+./main
+```
---
+++ c/memory/speed_test/main.c
@@ -0,0 +1,6 @@
+int main() {
+    int arr[1000000];
+    for (int i=0; i<1000000; i++) {
+        arr[i] = i + 1;
+    }
+}
