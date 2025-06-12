1db709dd1802a9bc482d91f7c06356ccb6cec701
netnode <grumirafi@gmail.com>
2025-06-12 19:55:55 +0200

    List vs linked list

--- python/DSA/1. Data Structures - Fundamentals/2. Single Linked Lists/main.py
+++ python/DSA/1. Data Structures - Fundamentals/2. Single Linked Lists/main.py
@@ -1,19 +1,26 @@
-from linked_list import ListNode, LinkedList
+from time import perf_counter_ns
+from linked_list import LinkedList
 
-head = ListNode('a')
-ll = LinkedList(head)
+def time_it(whois, fn, *args):
+    start = perf_counter_ns()
+    for _ in range(0, 100000):
+        fn(*args)
+    print(f"[{whois}] Time: {perf_counter_ns() - start} ns")
 
-def build_linked_list(ll: LinkedList):
-    for char in "bcdefgh":
-        ll.append(char)
+ll = LinkedList(0)
+for i in range(1, 101):
+    ll.append(i)
 
-build_linked_list(ll)
-ll.print()
+l = [ x for x in range(0, 101) ]
 
-new_head = ListNode("0")
-new_head.next = ll.head
-ll.head = new_head
-ll.print()
+print('\n(LL vs LIST) search')
+time_it('LL', ll.search, 50)
+time_it('LS', l.index, 50)
 
-ll.head = ll.head.next.next.next
-ll.print()
+print('\n(LL vs LIST) insert at head')
+time_it('LL', ll.insert_at_head, 101)
+time_it('LS', l.insert, 0, 101)
+
+print('\n(LL vs LIST) delete at head')
+time_it('LL', ll.delete_at_head)
+time_it('LS', l.pop, 0)
--- python/DSA/1. Data Structures - Fundamentals/timer.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def time_it(fn):
-    def _inner_dec():
-        start = time.perf_counter()
-        fn()
-        print(f"[*] Time: {time.perf_counter() - start}")
-    return _inner_dec
