277f6ac14b95dcf2af621a1eb1ca6b9269b47ce7
netnode <grumirafi@gmail.com>
2025-06-14 07:29:33 +0200

    Fix

--- c/memory/2. Memory Leak/leak.c
+++ c/memory/2. Memory Leak/leak.c
@@ -1,17 +1,17 @@
 #include <stdio.h> // printf, scanf
 #include <stdlib.h> // malloc
 
-void memory_leak() {
-    int n=0;
-    printf("Size of array? ");
-    scanf("%d", &n);
-    int *ptr = malloc(n * sizeof(int));  // (se int = 4 byte) Alloca (n * 4)byte
+void memory_leak(int n) {
+    int* ptr = malloc(n * sizeof(int));  // (se int = 4 byte) Alloca (n * 4)byte
     // ... usa ptr ...
     // Dimenticato free(ptr)!
 }  // ptr esce dallo scope, ma i (n * 4)byte restano bloccati
 
 int main() {
-    for(int i=0; i<1000; i++) {
-        fix_memory_leak();
+    int n=0;
+    printf("Size of array? ");
+    scanf("%d", &n);
+    for (int i=0; i<1000; i++) {
+        memory_leak(n);
     }
 }
